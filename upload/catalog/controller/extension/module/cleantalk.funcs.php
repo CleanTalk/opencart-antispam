74865bbd19560cbecaf664b64af4b15b0222c19e
davydovct <davydov@cleantalk.org>
2018-06-09 17:42:02 +0500

    fix ct_funcs

--- upload/catalog/controller/extension/module/cleantalk.funcs.php
+++ upload/catalog/controller/extension/module/cleantalk.funcs.php
@@ -1,21 +1,15 @@
 <?php
+require_once DIR_APPLICATION . '/controller/extension/module/cleantalk.class.php';
 
 class CleantalkFuncs
 {
 	const ENGINE = 'opencart-13';
 
 	private $ct_access_key = '';
-	
-	private $ct_status, $ct_check_register, $ct_check_orders, $ct_check_contact, $ct_check_reviews;
 
-	function __construct($access_key = '', $status = '0', $check_register = '0', $check_orders = '0', $check_contact = '0', $check_reviews = '0')
+	function __construct($access_key = '')
 	{
 		$this->ct_access_key = trim($access_key);
-		$this->ct_status = $status;
-		$this->ct_check_register = $check_register;
-		$this->ct_check_orders = $check_orders;
-		$this->ct_check_contact = $check_contact;
-		$this->ct_check_reviews = $check_reviews;
 	}
 	
 	public function setCookie()
@@ -45,88 +39,80 @@ class CleantalkFuncs
 	{
         $ret_val = array();
         $ret_val['allow'] = 1;		
-	    if ($this->ct_status)
-	    {
-	        require_once DIR_APPLICATION . '/controller/extension/module/cleantalk.class.php';
-	        $refferrer = null;
-	        if (isset($_SERVER['HTTP_REFERER'])) {
-	            $refferrer = htmlspecialchars((string) $_SERVER['HTTP_REFERER']);
-	        }
 
-	        $user_agent = null;
-	        if (isset($_SERVER['HTTP_USER_AGENT'])) {
-	            $user_agent = htmlspecialchars((string) $_SERVER['HTTP_USER_AGENT']);
-	        }
-	        $sender_info = array(
-	            'REFFERRER' => $refferrer,
-	            'post_url' => $refferrer,
-	            'USER_AGENT' => $user_agent,
-	            'REFFERRER_PREVIOUS' => isset($_COOKIE['apbct_prev_referer'])?$_COOKIE['apbct_prev_referer']:null,
-	        );
-	        $js_on = 0;
-	        if (isset($_POST['ct_checkjs']) && $_POST['ct_checkjs'] == date("Y"))
-	            $js_on = 1;
-	        $sender_info = json_encode($sender_info);   
-	        $ct = new Cleantalk();
-	        $ct->work_url = 'http://moderate.cleantalk.org';
-	        $ct->server_url = 'http://moderate.cleantalk.org';	        
-	        $ct_request = new CleantalkRequest();
-	        $ct_request->auth_key = $this->ct_access_key;
-	        $ct_request->sender_email = $data['email'];
-	        $ct_request->sender_ip = $ct->cleantalk_get_real_ip();
-	        $ct_request->agent = self::ENGINE;
-	        $ct_request->js_on = $js_on;
-	        $ct_request->sender_info = $sender_info;
-	        $ct_request->submit_time = time() - intval($_COOKIE['apbct_timestamp']);
-	        $post_info['comment_type'] = $content_type;
-			$ct_request->post_info = $post_info;	        
-	        switch ($content_type)
-	        {
-	        	case 'register':
-	        		$ct_request->sender_nickname = trim($data['firstname']).' '.trim($data['lastname']);
-			        if ($this->ct_check_register)
-			            $ct_result = $ct->isAllowUser($ct_request);                
-			        break;
-			    case 'order':
-			    	$ct_request->sender_nickname = trim($data['firstname']).' '.trim($data['lastname']);
+        $refferrer = null;
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $refferrer = htmlspecialchars((string) $_SERVER['HTTP_REFERER']);
+        }
 
-	                if ($this->ct_check_orders)
-	                    $ct_result = $ct->isAllowUser($ct_request);
-	                break;
-	            case 'contact':
-	            	$ct_request->sender_nickname = trim($data['name']);
-	            	$ct_request->message = trim($data['enquiry']);
-			    	if ($this->ct_check_contact)
-			    		$ct_result = $ct->isAllowMessage($ct_request);
-			    	break;
-			    case 'comment':
-			    	$ct_request->sender_nickname = trim($data['name']);
-			    	$ct_request->message = trim($data['text']);
-			    	if ($this->ct_check_reviews)
-			    		$ct_result = $ct->isAllowMessage($ct_request);
-			    	break;
-	        }
-	        if ($ct_result)
-	        {
-	        	if ($ct_result->errno != 0){
-	        		//TODO: inform admin
-	        		$ret_val['errno'] = $ct_result->errno;
-	        		if ($js_on == 1)
-	        		{
-	        			$ret_val['allow'] = 0;
-	        			$ret_val['comment'] = 'Cleantalk. Javascript Disabled';
-	        		}
-	        	}
-	        	else
+        $user_agent = null;
+        if (isset($_SERVER['HTTP_USER_AGENT'])) {
+            $user_agent = htmlspecialchars((string) $_SERVER['HTTP_USER_AGENT']);
+        }
+        $sender_info = array(
+            'REFFERRER' => $refferrer,
+            'post_url' => $refferrer,
+            'USER_AGENT' => $user_agent,
+            'REFFERRER_PREVIOUS' => isset($_COOKIE['apbct_prev_referer'])?$_COOKIE['apbct_prev_referer']:null,
+        );
+        $js_on = 0;
+        if (isset($_POST['ct_checkjs']) && $_POST['ct_checkjs'] == date("Y"))
+            $js_on = 1;
+        $sender_info = json_encode($sender_info);   
+        $ct = new Cleantalk();
+        $ct->work_url = 'http://moderate.cleantalk.org';
+        $ct->server_url = 'http://moderate.cleantalk.org';	        
+        $ct_request = new CleantalkRequest();
+        $ct_request->auth_key = $this->ct_access_key;
+        $ct_request->sender_email = $data['email'];
+        $ct_request->sender_ip = $ct->cleantalk_get_real_ip();
+        $ct_request->agent = self::ENGINE;
+        $ct_request->js_on = $js_on;
+        $ct_request->sender_info = $sender_info;
+        $ct_request->submit_time = time() - intval($_COOKIE['apbct_timestamp']);
+        $post_info['comment_type'] = $content_type;
+		$ct_request->post_info = $post_info;	        
+        switch ($content_type)
+        {
+        	case 'register':
+        		$ct_request->sender_nickname = trim($data['firstname']).' '.trim($data['lastname']);
+		        $ct_result = $ct->isAllowUser($ct_request);                
+		        break;
+		    case 'order':
+		    	$ct_request->sender_nickname = trim($data['firstname']).' '.trim($data['lastname']);
+                $ct_result = $ct->isAllowUser($ct_request);
+                break;
+            case 'contact':
+            	$ct_request->sender_nickname = trim($data['name']);
+            	$ct_request->message = trim($data['enquiry']);
+		    	$ct_result = $ct->isAllowMessage($ct_request);
+		    	break;
+		    case 'comment':
+		    	$ct_request->sender_nickname = trim($data['name']);
+		    	$ct_request->message = trim($data['text']);
+		    	$ct_result = $ct->isAllowMessage($ct_request);
+		    	break;
+        }
+        if (isset($ct_result))
+        {
+        	if ($ct_result->errno != 0){
+        		//TODO: inform admin
+        		$ret_val['errno'] = $ct_result->errno;
+        		if ($js_on == 1)
+        		{
+        			$ret_val['allow'] = 0;
+        			$ret_val['comment'] = 'Cleantalk. Javascript Disabled';
+        		}
+        	}
+        	else
+        	{
+	        	if ($ct_result->allow == 0)
 	        	{
-		        	if ($ct_result->allow == 0)
-		        	{
-		        		$ret_val['allow'] = 0;
-		        		$ret_val['comment'] = $ct_result->comment;
-		        	}
+	        		$ret_val['allow'] = 0;
+	        		$ret_val['comment'] = $ct_result->comment;
 	        	}
-	        }          
-	    }
+        	}
+        }          
 	    
 		return $ret_val; 	    		
 	}
